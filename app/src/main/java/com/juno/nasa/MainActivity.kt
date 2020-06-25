47b8bc13b6c8a0826a2b6f687daaaf31eefea86f
Sreekanth <sreekanth100k@gmail.com>
2020-06-25 14:23:35 +0530

    Initial commit

---
+++ app/src/main/java/com/juno/nasa/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.juno.nasa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        id_play_or_zoom_btn.setOnClickListener(View.OnClickListener {
+
+        })
+
+    }
+}

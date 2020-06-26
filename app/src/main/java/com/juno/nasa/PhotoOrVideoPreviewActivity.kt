c4b185a8df371ee0f51685eeee374968bf62e6c9
Sreekanth <sreekanth100k@gmail.com>
2020-06-26 22:50:24 +0530

    Some attempts to load video

--- app/src/main/java/com/juno/nasa/PhotoOrVideoPreviewActivity.kt
+++ app/src/main/java/com/juno/nasa/PhotoOrVideoPreviewActivity.kt
@@ -1,16 +1,22 @@
 package com.juno.nasa
 
 import android.app.ProgressDialog
+import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.MediaController
 import android.widget.Toast
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import kotlinx.android.synthetic.main.activity_main.id_iv
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class PhotoOrVideoPreviewActivity:AppCompatActivity() {
 
@@ -24,10 +30,25 @@ class PhotoOrVideoPreviewActivity:AppCompatActivity() {
         var url: String?         = intent.getStringExtra("url")
         var photoOrVideo:String? = intent.getStringExtra("photoOrVideo")
 
+        photoOrVideo  = "video"
+
         if(photoOrVideo.equals("photo")){
             Toast.makeText(this,"Photo",Toast.LENGTH_SHORT).show()
         }else if(photoOrVideo.equals("video")){
             Toast.makeText(this,"Video",Toast.LENGTH_SHORT).show()
+
+            id_iv.visibility = View.GONE
+
+//            val videoView = findViewById<View>(R.id.id_video_view) as VideoView
+//            val controller = MediaController(this)
+//            videoView.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=4-BiDcw4CjY"))
+//            videoView.setMediaController(controller)
+//            videoView.requestFocus();
+//            videoView.start()
+
+            val intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=4-BiDcw4CjY"))
+            startActivity(intent)
         }
 
 
@@ -56,7 +77,7 @@ class PhotoOrVideoPreviewActivity:AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
 
-        overridePendingTransition( R.anim.no_animation, R.anim.slide_out_from_left_top );
+//        overridePendingTransition( R.anim.no_animation, R.anim.slide_out_from_left_top );
 
     }
 }
